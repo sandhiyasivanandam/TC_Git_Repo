4f1d7388dccc02a20985e6e896e95d6bfffbd58a
Sandhiya Sivanandam <70689445+sandhiyasivanandam@users.noreply.github.com>
2024-07-25 22:16:56 +0530

    Update Unit1.js

--- TestProject80/Script/Unit1.js
+++ TestProject80/Script/Unit1.js
@@ -24,6 +24,4 @@
   Aliases.browser.pageTestcomplete15DocumentationT2.textnodeTestcomplete1564.Click();
   //adding a message
   Log.Message("added from TestComplete");
-  //adding another message
-  log.message("added  from git repo");
-}
+ }
